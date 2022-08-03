b928d5790f14644f7b14137181acf27ad8717892
Anael Jonas <anaeljonas@outlook.com>
2022-08-03 15:29:00 -0300

    [feature] Help

--- src/server.js
+++ src/server.js
@@ -6,11 +6,16 @@ const env = require('./env');
 
 const app = express();
 app.use(cors());
+app.use('/assets', express.static(__dirname+'/assets'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/pages');
 
 app.get('/', (req, res) => {
     res.render('debugger');
+});
+
+app.get('/help', (req, res) => {
+    res.render('help');
 });
 
 app.use((req, res, next) => {
